c99d21dc567f43d75e993938672517bebd5d8f81
UWUpip <77720597+migerweb@users.noreply.github.com>
2024-12-02 22:30:52 +0300

    Add files via upload

---
+++ Programm.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualBasic;
+using System.Windows.Forms;
+using System;
+
+class Hell
+{
+    static void Main()
+    {
+        Console.Title = "Данные";
+        Console.Write("Введи что-нибудь: ");
+
+        string name;
+
+        name = Console.ReadLine();
+
+        Console.WriteLine("Понял "+name+"");
+    }
+}
